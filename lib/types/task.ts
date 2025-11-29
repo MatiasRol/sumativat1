78ca837fa1a6cd957d4e71d2f4b1257f9fac270f
roldanmatias441 <roldanmatias441@gmail.com>
2025-11-29 03:18:30 +0000

    ✨ feat(structure): crear estructura base de carpetas y tipos

---
+++ lib/types/task.ts
@@ -0,0 +1,10 @@
+export interface Task {
+    id: string;
+    title: string;
+    description: string;
+    completed: boolean;
+    createdAt: string;
+  }
+  
+  export type CreateTaskDto = Omit<Task, 'id' | 'createdAt' | 'completed'>;
+  export type UpdateTaskDto = Partial<CreateTaskDto>;
